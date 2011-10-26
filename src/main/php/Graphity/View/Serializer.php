2ba090f8f2608f72f676816fddd182695e5e40e0
pumba_lt <pumba@xml.lt>
2011-10-26 15:56:47 +0200

    Moved RDF/XML methods

--- src/main/php/Graphity/View/Serializer.php
+++ src/main/php/Graphity/View/Serializer.php
@@ -28,33 +28,66 @@ namespace HeltNormalt\View;
  */
 abstract class Serializer
 {
+	protected static function createDOMModel(Model $model, \DOMNode $parent)
+	{
+        if ($parent instanceof \DOMDocument) $doc = $parent;
+        else $doc = $parent->ownerDocument;
 
-    /**
-     * Serializes objects into XML strings.
-     * @param mixed $mixed Object to serialize
-     * @return string XML string
-     */
-    
-    public static function serialize($mixed)
-    {
-        $xml = null;
-        
-        //if (is_array($mixed)) return self::serializeArray($mixed);
-        
-
-        if($mixed instanceof Resource)
-            return self::serializeResource($mixed);
+		return $parent->appendChild($doc->createElementNS(Rdf::NS, "rdf:RDF"));
     }
 
-    /**
-     * Serializes Resource objects into XML strings.
-     * @param Resource $resource Resource object to serialize
-     * @return string XML string
-     */
-    
-    protected static function serializeResource(Resource $resource)
+	protected static function createDOMModelChildren(Model $model, \DOMElement $rdf)
+	{
+        foreach ($model->getStatements() as $stmt)
+            self::createDOMStmt($stmt, $rdf);
+    }
+
+    protected static function createDOMStmt(Statement $stmt, \DOMElement $rdf)
     {
-        return "<" . get_class($resource) . " uri=\"" . $resource->getURI() . "\"/>";
+        //return $rdf->appendChild(
+            self::createDOMObject($stmt->getObject(),
+                self::createDOMPredicate($stmt->getPredicate(),
+                    self::createDOMSubject($stmt->getSubject(), $rdf)));
+    }
+
+    protected static function createDOMSubject(Graphity\Rdf\Resource $subject, \DOMElement $stmt)
+    {
+        $descElem = $stmt->ownerDocument->createElementNS(Rdf::NS, "rdf:Description");
+        if ($subject->isAnonymous()) $descElem->setAttributeNS(Rdf::NS, "rdf:nodeID", $subject->getAnonymousId());
+        else $descElem->setAttributeNS(Rdf::NS, "rdf:about", $subject->getURI());
+        return $stmt->appendChild($descElem);
+    }
+
+    protected static function createDOMPredicate(Graphity\Rdf\Resource $predicate, \DOMElement $subject)
+    {
+        $nsUri = $localName = null;
+        // try predicate local name as substring after #
+        if (strrpos($predicate->getURI(), "#") !== false)
+        {
+            $nsUri = substr($predicate->getURI(), 0, strrpos($predicate->getURI(), "#") + 1);
+            $localName = substr($predicate->getURI(), strrpos($predicate->getURI(), "#") + 1);
+        }
+        // as a 2nd option try predicate local name as substring after /
+        if ($nsUri === null && $localName === null && strrpos($predicate->getURI(), "/") !== false)
+        {
+            $nsUri = substr($predicate->getURI(), 0, strrpos($predicate->getURI(), "/") + 1);
+            $localName = substr($predicate->getURI(), strrpos($predicate->getURI(), "/") + 1);
+        }
+        return $subject->appendChild($subject->ownerDocument->createElementNS($nsUri, "tmp" . ":" . $localName)); // uniqid() didn't work
+    }
+
+    protected static function createDOMObject(Graphity\Rdf\Node $object, \DOMElement $predicate)
+    {
+        if ($object->isLiteral())
+        {
+            if ($object->hasDatatype()) $predicate->setAttributeNS(Rdf::NS, "rdf:datatype", $object->getDatatype());
+            return $predicate->appendChild($predicate->ownerDocument->createTextNode($object->getValue()));
+        }
+        if ($object->isResource())
+        {
+            if ($object->isAnonymous()) $predicate->setAttributeNS(Rdf::NS, "rdf:nodeID", $object->getAnonymousId());
+            if ($object->isURIResource()) $predicate->setAttributeNS(Rdf::NS, "rdf:resource", $object->getURI());
+        }
     }
 
 }
