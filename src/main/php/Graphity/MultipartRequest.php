afa1de584d5e4d1ad93c21272d08367bd24d63ae
pumba_lt <pumba@xml.lt>
2011-11-29 12:01:34 +0100

    MultipartRequest implements Request

--- src/main/php/Graphity/MultipartRequest.php
+++ src/main/php/Graphity/MultipartRequest.php
@@ -44,12 +44,14 @@ class MultipartRequest implements RequestInterface
      * @param string $saveDir Directory to save files to
      */
     
-    public function __construct(Request $request, $saveDir, $maxSize = DEFAULT_MAX_POST_SIZE, $encoding = null)
+    public function __construct(Request $request, $saveDir, $maxSize = null, $encoding = null)
     {
         if ($request == null)
             throw new Exception("$request cannot be null");
         if ($saveDir == null)
             throw new Exception("saveDirectory cannot be null");
+        if ($maxSize === null)
+            $maxSize = self::DEFAULT_MAX_POST_SIZE;
         if ($maxSize <= 0)
             throw new Exception("maxPostSize must be positive");
         $this->request = $request;
@@ -137,6 +139,93 @@ class MultipartRequest implements RequestInterface
             return null;
         }
     }
+
+    public function getAttribute($name)
+    {
+        return $this->request->getAttribute($name);
+    }
+
+    public function setAttribute($name, $value)
+    {
+        $this->request->setAttribute($name, $value);
+    }
+
+    public function getContentType()
+    {
+        return $this->request->getContentType();
+    }
+
+    public function getContentLength()
+    {
+        return $this->request->getContentLength();
+    }
+
+    public function getCookies()
+    {
+        return $this->request->getCookies();
+    }
+
+    public function getParameter($name)
+    {
+        return $this->request->getParameter($name);
+    }
+
+    public function getMethod()
+    {
+        return $this->request->getMethod();
+    }
+
+    public function getHeader($name)
+    {
+        return $this->request->getHeader($name);
+    }
+
+    public function getSession()
+    {
+        return $this->request->getSession();
+    }
+
+    public function getParameterMap()
+    {
+        return $this->request->getParameterMap();
+    }
+
+    public function getPathInfo()
+    {
+        return $this->request->getPathInfo();
+    }
+
+    public function getRequestURI()
+    {
+        return $this->request->getRequestURI();
+    }
+
+    public function getServerName()
+    {
+        return $this->request->getServerName();
+    }
+
+    public function getServerPort()
+    {
+        return $this->request->getServerPort();
+    }
+
+    public function getScheme()
+    {
+        return $this->request->getScheme();
+    }
+
+    public function getQueryString()
+    {
+        return $this->request->getQueryString();
+    }
+
+	public function getInputStream()
+    {
+        return $this->request->getInputStream();
+    }
+
+
 }
 
 ?>
