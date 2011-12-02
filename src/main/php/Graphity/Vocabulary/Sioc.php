0ad4b7fd06521927573c1a8d46ac86eedeeb42b0
pumba_lt <pumba@xml.lt>
2011-12-02 18:16:51 +0100

    Added FOAF & SIOC

---
+++ src/main/php/Graphity/Vocabulary/Sioc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *  Copyright 2011 Graphity Team
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  @package        graphity
+ *  @author         Martynas Jusevičius <pumba@xml.lt>
+ *  @link           http://graphity.org/
+ */
+
+namespace Graphity\Vocabulary;
+
+class Sioc
+{
+
+    const NS = "http://rdfs.org/sioc/ns#";
+
+    const Site = "http://rdfs.org/sioc/ns#Site";
+
+    const Forum = "http://rdfs.org/sioc/ns#Forum";
+
+    const Post = "http://rdfs.org/sioc/ns#Post";
+
+    const has_creator = "http://rdfs.org/sioc/ns#has_creator";
+
+    const comment = "http://rdfs.org/sioc/ns#comment";
+
+    const has_container = "http://rdfs.org/sioc/ns#has_container";
+
+    const content = "http://rdfs.org/sioc/ns#content";
+
+    const num_items = "http://rdfs.org/sioc/ns#num_items";
+
+}
