b3d9e7bc90ad57c89b395c50f76afddf4c040af6
Aleksandras Smirnovas <aleksandras@coder.lt>
2012-01-12 13:43:33 +0200

    group-triples fix

--- src/main/php/Graphity/Repository/Repository.php
+++ src/main/php/Graphity/Repository/Repository.php
@@ -126,6 +126,7 @@ class Repository implements RepositoryInterface
         // make sure triples are grouped by resource uri.
         $doc = new \DOMDocument("1.0", "UTF-8");
         $doc->loadXML($result);
+        
         $result= XSLTBuilder::fromStylesheetURI((dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "webapp" . DIRECTORY_SEPARATOR . "WEB-INF" . DIRECTORY_SEPARATOR . "xsl" . DIRECTORY_SEPARATOR . "group-triples.xsl"))->document($doc)->buildXML();
 
         return $result;
