296753216ae3e8d18009862e59fb4c0bb0e71aec
pumba_lt <pumba@xml.lt>
2012-01-21 20:32:45 +0100

    XSD::decimal

--- src/main/php/Graphity/Vocabulary/XSD.php
+++ src/main/php/Graphity/Vocabulary/XSD.php
@@ -30,7 +30,9 @@ class XSD
     const int = "http://www.w3.org/2001/XMLSchema#int";
 
     const integer = "http://www.w3.org/2001/XMLSchema#integer";
-    
+
+    const decimal = "http://www.w3.org/2001/XMLSchema#integer";
+
     const string = "http://www.w3.org/2001/XMLSchema#string";
 
     const dateTime = "http://www.w3.org/2001/XMLSchema#dateTime";
