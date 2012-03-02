4d63fd194049de07b255e7d3a46ccc3531bf92fe
Julius Seporaitis <julius@seporaitis.net>
2012-03-02 18:08:29 +0200

    Removed PhutilURI dependency

--- src/main/php/Graphity/Util/UriBuilder.php
+++ src/main/php/Graphity/Util/UriBuilder.php
@@ -24,141 +24,164 @@ namespace Graphity\Util;
 
 /**
  * Uri builder class for easier composition of URIs.
- * 
+ *
  * Example usage:
- * 
+ *
  * UriBuilder::newInstance()->host("www.seporaitis.net")->path("index.php/2010/01/20/sis-zurnalas-uzsidaro/")->fragment("comments").build();
- * 
+ *
  * Will return:
- * 
+ *
  * "http://www.seporaitis.net/index.php/2010/01/20/sis-zurnalas-uzsidaro/#comments"
- * 
+ *
  * Based on this interface: http://jsr311.java.net/nonav/javadoc/javax/ws/rs/core/UriBuilder.html
- *  
+ *
  * Note that this implementation ignores regular expressions inside { }.
  */
 class UriBuilder {
-    
+
     const _REGEXP_SCHEMA = '/(\w+):\/\//i';
     const _REGEXP_PORT = '/:(\d+)[\/]?/i';
     const _REGEXP_HOST = '/\w+:\/\/([\w\.\-\d]+)/i';
-    
+
     /**
      * @var string
      */
     protected $scheme = null;
-    
+
     /**
      * @var string
      */
     protected $hostname = null;
-    
+
     /**
      * @var int
      */
     protected $port = null;
-    
+
     /**
      * @var array
      */
     protected $listOfSegments = array();
-    
+
     /**
      * @var array
      */
     protected $listOfQueryParams = array();
-    
+
     /**
      * @var string
      */
     protected $fragment = null;
-    
+
     public function __construct() {
         // empty
     }
-    
+
     /**
      * Create a new instance
-     * 
+     *
      * @return UriBuilder
      */
     public static function newInstance() {
         $className = get_called_class();
         return new $className;
     }
-    
+
     /**
      * Create a new instance from an existing URI.
-     * 
-     * @param mixed $uri
-     * 
+     *
+     * @param string $uri
+     *
      * @return UriBuilder
      */
     public static function fromUri($uri) {
-        if(is_string($uri)) {
-            $uri = new PhutilURI($uri);
-        }
-        
         $className = get_called_class();
         $builder = new $className;
-        
-        $builder->scheme($uri->getProtocol());
-        $builder->host($uri->getDomain());
-        $builder->port($uri->getPort() == 80 ? null : $uri->getPort());
-        
-        $builder->path($uri->getPath());
-        
-        foreach($uri->getQueryParams() as $name => $value) {
-            $builder->queryParam($name, $value);
+
+        $components = parse_url($uri);
+
+        if(array_key_exists('scheme', $components)) {
+            $builder->scheme($components['scheme']);
         }
-        
-        $builder->fragment($uri->getFragment());
-        
+
+        if(array_key_exists('host', $components)) {
+            $builder->host($components['host']);
+        }
+
+        if(array_key_exists('port', $components)) {
+            $builder->port($components['port']);
+        }
+
+        if(array_key_exists('path', $components)) {
+            $builder->path($components['path']);
+        }
+
+        if(array_key_exists('query', $components)) {
+            $queryParams = array();
+            if(strlen($components['query']) > 0) {
+                parse_str($components['query'], $queryParams);
+            }
+
+            foreach($queryParams as $name => $value) {
+                $builder->queryParam($name, $value);
+            }
+        }
+
+        if(array_key_exists('fragment', $components)) {
+            $builder->fragment($components['fragment']);
+        }
+
         return $builder;
     }
-    
+
     /**
      * Create an UriBuilder representing relative URI initialized from URI path.
-     * 
+     *
      * @param string $path
-     * 
+     *
      * @return UriBuilder
      */
     public static function fromPath($path) {
         $className = get_called_class();
         $builder = new $className;
-        
-        $partMap = parse_url($path);
-        
-        $builder->listOfSegments = explode("/", $partMap['path']);
-        if(array_key_exists('fragment', $partMap)) {
-            $builder->fragment($partMap['fragment']);
+
+        $components = parse_url($path);
+
+        if(array_key_exists('path', $components)) {
+            $builder->listOfSegments = explode("/", $components['path']);
         }
-        
-        if(array_key_exists('query', $partMap)) {
-            $nvPairs = explode("&", $partMap['query']);
-            foreach($nvPairs as $pair) {
-                $parts = explode("=", $pair);
-                $builder->queryParam($parts[0], $parts[1]);
+
+        if(array_key_exists('fragment', $components)) {
+            $builder->fragment($components['fragment']);
+        }
+
+        if(array_key_exists('query', $components)) {
+            $queryParams = array();
+            if(strlen($components['query']) > 0) {
+                parse_str($components['query'], $queryParams);
+            }
+
+            foreach($queryParams as $name => $value) {
+                $builder->queryParam($name, $value);
             }
         }
-        
+
         return $builder;
     }
-    
+
     /**
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function scheme($value) {
         $this->scheme = $value;
         return $this;
     }
-    
+
     /**
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function host($value) {
@@ -172,23 +195,23 @@ class UriBuilder {
             }
             $this->hostname = $host;
         }
-        
+
         return $this;
     }
-    
+
     /**
      * @param int $value
-     * 
+     *
      * @return UriBuilder
      */
     public function port($value) {
         $this->port = $value;
         return $this;
     }
-    
+
     /**
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function path($value) {
@@ -196,13 +219,13 @@ class UriBuilder {
         foreach($list as $segment) {
             $this->segment($segment);
         }
-        
+
         return $this;
     }
 
     /**
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function replacePath($value) {
@@ -215,98 +238,98 @@ class UriBuilder {
 
     /**
      * Append a single path segment
-     * 
+     *
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function segment($value) {
         if(empty($value)) {
             return $this;
         }
-        
+
         $this->listOfSegments[] = $value;
         return $this;
     }
-    
+
     /**
      * Add a query name=value pair
-     * 
+     *
      * @param string $name
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function queryParam($name, $value) {
         $this->listOfQueryParams[$name] = $value;
         return $this;
     }
-    
+
     /**
      * @param string $value
-     * 
+     *
      * @return UriBuilder
      */
     public function fragment($value) {
         $this->fragment = $value;
         return $this;
     }
-    
+
     /**
      * Build URI from created template
-     * 
+     *
      * Example usage:
-     * 
+     *
      * UriBuilder::newInstance()->host("coolwebsite.com")->path("/{lang}/news/{id}")->build("en", 1234);
-     * 
+     *
      * Will return
-     * 
+     *
      * "http://coolwebsite.com/en/news/1234"
-     * 
+     *
      * @param mixed $param1, $param2, ...
-     *  
+     *
      * @returns string
      */
     public function build() {
-        
+
         $paramMap = func_get_args();
-        
+
         return $this->buildWithTemplate(function($value) use (&$paramMap) {
             $startPos = strpos($value, "{");
             if($startPos === false) {
                 return $value;
             }
-            
+
             $endPos = strrpos($value, "}");
             if($endPos === false) {
                 return $value;
             }
-            
+
             $key = substr($value, $startPos + 1, $endPos - 1);
             if(strpos($key, ":") !== false) {
                 $key = substr($key, 0, strpos($key, ":"));
             }
-            
+
             $newValue = array_shift($paramMap);
             if($newValue === null) {
                 return $value;
             }
-            
+
             return str_ireplace("{" . $key . "}", $newValue, $value);
         });
     }
-    
+
     /**
      * Build URI from created template using parameter map.
-     * 
+     *
      * Example usage:
-     * 
+     *
      * UriBuilder::newInstance()->host("coolwebsite.com")->path("/{lang}/news/{id}")->buildFromMap(array('lang' => "en", 'id' => 1234));
-     * 
+     *
      * Will return
-     * 
+     *
      * "http://coolwebsite.com/en/news/1234"
-     * 
+     *
      * @param array $paramMap
      * @returns string
      */
@@ -315,45 +338,45 @@ class UriBuilder {
             if(strpos($value, "{") === false) {
                 return $value;
             }
-            
+
             foreach($paramMap as $key => $newValue) {
                 $value = str_ireplace("{" . $key . "}", $newValue, $value);
             }
-            
+
             return $value;
         });
     }
-    
+
     /**
      * Actual Uri builder.
-     * 
+     *
      * Takes an anonymous function $template and applies it to all parts of
      * the uri, before returning it.
-     * 
+     *
      * @param function $template
-     * 
+     *
      * @return string
      */
     protected function buildWithTemplate($template) {
         $uri = "";
-        
+
         if($this->scheme !== null) {
             $uri .= $this->scheme . "://";
         }
-        
+
         if($this->hostname !== null) {
             if(strlen($uri) === 0) {
                 $uri .= "http://";
             }
             $uri .= $template($this->hostname);
         }
-        
+
         if(!empty($this->port)) {
             if($this->scheme !== null && (($this->scheme == "http" && intval($this->port) !== 80) || ($this->scheme == "https" && intval($this->port) !== 443))) {
                 $uri .= ":" . $this->port;
             }
         }
-        
+
         if(strlen($uri) > 0) {
             if(array_key_exists(0, $this->listOfSegments)) {
                 if(!empty($this->listOfSegments[0])) {
@@ -365,11 +388,11 @@ class UriBuilder {
                 $uri .= "/";
             }
         }
-        
+
         foreach($this->listOfSegments as $key => $segment) {
             $uri .= urlencode($template($segment)) . (($key < count($this->listOfSegments) - 1) ? "/" : "");
         }
-        
+
         if(count($this->listOfQueryParams) > 0) {
             $uri .= "?";
             $counter = 0;
@@ -378,11 +401,11 @@ class UriBuilder {
                 $counter++;
             }
         }
-        
+
         if($this->fragment !== null) {
             $uri .= "#" . urlencode($template($this->fragment));
         }
-        
+
         return $uri;
     }
 }
